f45f3a008938cc55b8802361299c5e37ce55030e
secret-dev-ninja <luckydev077@gmail.com>
2024-11-25 20:30:20 -0800

    feat: set the new value on the env file

--- frontend/env.d.ts
+++ frontend/env.d.ts
@@ -2,5 +2,6 @@ namespace NodeJS {
     interface ProcessEnv {
         REACT_APP_WS_URL: string;
         REACT_APP_API_URL: string;
+        REACT_APP_CHART_API_URL: string;
     }
 }
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -11,6 +11,7 @@ module.exports = {
   },
   env: {
     REACT_APP_WS_URL: process.env.REACT_APP_WS_URL,
-    REACT_APP_API_URL: process.env.REACT_APP_API_URL
+    REACT_APP_API_URL: process.env.REACT_APP_API_URL,
+    REACT_APP_CHART_API_URL: process.env.REACT_APP_CHART_API_URL
   }
 }
